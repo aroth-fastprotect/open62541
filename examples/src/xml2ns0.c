f25a7987e2edd46cf12ada50d97e62310b24db29
Leon Urbas <leon.urbas@tu-dresden.de>
2014-05-09 07:12:38 +0200

    application data resides in Variant of VariableNode!

--- examples/src/xml2ns0.c
+++ examples/src/xml2ns0.c
@@ -9,75 +9,79 @@
 
 // FIXME: most of the following code should be generated as a template from the ns0-defining xml-files
 /** @brief server_application_memory is a flattened memory structure of the UAVariables in ns0 */
-struct server_application_memory {
-	UA_UtcTime startTime;
-	UA_UtcTime currentTime;
-	UA_ServerState state;
-	UA_String productName;
-	UA_String productUri;
-	UA_String manufacturerName;
-	UA_String softwareVersion;
-	UA_String buildNumber;
-	UA_String buildDate;
-	UA_Int32 secondsTillShutdown;
-	UA_String shutdownReason;
-} server_application_memory;
+struct sam {
+	UA_ServerStatusDataType serverStatus;
+} sam;
 
 char* productName = "xml2ns0";
 char* productUri = "http://open62541.org/xml2ns0/";
 char* manufacturerName = "open62541";
 char* softwareVersion = "0.01";
 char* buildNumber = "999" __DATE__ "-001" ;
-char* buildDate = __DATE__;
 
-#define SAM_ASSIGN_CSTRING(src) do { \
-	server_application_memory.src.length = strlen(src)-1; \
-	server_application_memory.src.data = (UA_Byte*) src; \
+#define SAM_ASSIGN_CSTRING(src,dst) do { \
+	dst.length = strlen(src)-1; \
+	dst.data = (UA_Byte*) src; \
 } while(0)
 
 void* direct_access_strategy(void* arg) { return arg; }
 UA_DateTime* setGetCurrentTime(UA_DateTime* arg) { *arg = UA_DateTime_now(); return arg; }
 
-#define MAP_NODEID_TO_METHOD(ns,id,f,name) \
-		Namespace_attachData(ns, id, (Namespace_access_strategy) f, &server_application_memory.name, UA_NULL)
-
-#define MAP_NODEID_TO_ADDR(ns,id,name) \
-		MAP_NODEID_TO_METHOD(ns,id, direct_access_strategy,name)
-
-void server_application_memory_init(Namespace* ns) {
-	// Initialize the strings
-	SAM_ASSIGN_CSTRING(productName);
-	SAM_ASSIGN_CSTRING(productUri);
-	SAM_ASSIGN_CSTRING(manufacturerName);
-	SAM_ASSIGN_CSTRING(manufacturerName);
-	SAM_ASSIGN_CSTRING(softwareVersion);
-	SAM_ASSIGN_CSTRING(buildNumber);
-	SAM_ASSIGN_CSTRING(buildDate);
-
-	// Attach memory
-	MAP_NODEID_TO_ADDR(ns,2256,startTime); // this is the head of server status!
-	MAP_NODEID_TO_ADDR(ns,2257,startTime);
-	MAP_NODEID_TO_METHOD(ns,2258,setGetCurrentTime,currentTime);
-	MAP_NODEID_TO_ADDR(ns,2259,state);
-	MAP_NODEID_TO_ADDR(ns,2260,productName); // start of build Info
-	MAP_NODEID_TO_ADDR(ns,2261,productName);
-	MAP_NODEID_TO_ADDR(ns,2262,productUri);
-	MAP_NODEID_TO_ADDR(ns,2263,manufacturerName);
-	MAP_NODEID_TO_ADDR(ns,2264,softwareVersion);
-	MAP_NODEID_TO_ADDR(ns,2265,buildNumber);
-	MAP_NODEID_TO_ADDR(ns,2266,buildDate);
-	MAP_NODEID_TO_ADDR(ns,2292,secondsTillShutdown);
-	MAP_NODEID_TO_ADDR(ns,2293,shutdownReason);
+void sam_attach(Namespace *ns,UA_Int32 id,UA_Int32 type, void* p) {
+	Namespace_Lock* lock;
+	UA_NodeId nodeid;
+	nodeid.namespace = ns->namespaceId;
+	nodeid.identifier.numeric = id;
+	nodeid.encodingByte = UA_NODEIDTYPE_FOURBYTE;
+	UA_Node* result;
+	Namespace_getWritable(ns,&nodeid,&result,&lock);
+	if (result->nodeClass == UA_NODECLASS_VARIABLE) {
+		UA_VariableNode* variable = (UA_VariableNode*) result;
+		variable->value.arrayLength = 1;
+		// FIXME: maybe we want to make a difference for an array and a scalar value
+		if (variable->value.data == UA_NULL) {
+			UA_alloc((void**)&(variable->value.data), sizeof(void*));
+		}
+		variable->value.data[0] = p;
+		variable->value.encodingMask = UA_[type].ns0Id;
+		variable->value.vt = &UA_[type];
+	}
+	Namespace_Lock_release(lock);
 }
 
-UA_Int16 UA_NodeId_getNamespace(UA_NodeId* id) {
+void sam_init(Namespace* ns) {
+	// Initialize the strings
+	SAM_ASSIGN_CSTRING(productName,sam.serverStatus.buildInfo.productName);
+	SAM_ASSIGN_CSTRING(productUri,sam.serverStatus.buildInfo.productUri);
+	SAM_ASSIGN_CSTRING(manufacturerName,sam.serverStatus.buildInfo.manufacturerName);
+	SAM_ASSIGN_CSTRING(softwareVersion,sam.serverStatus.buildInfo.softwareVersion);
+	SAM_ASSIGN_CSTRING(buildNumber,sam.serverStatus.buildInfo.buildNumber);
+
+	// Attach memory
+	sam_attach(ns,2256,UA_SERVERSTATUSDATATYPE,&sam.serverStatus); // this is the head of server status!
+	sam_attach(ns,2257,UA_DATETIME, &sam.serverStatus.startTime);
+	sam_attach(ns,2258,UA_DATETIME, &sam.serverStatus.currentTime);
+	sam_attach(ns,2259,UA_SERVERSTATE, &sam.serverStatus.state);
+	sam_attach(ns,2260,UA_BUILDINFO, &sam.serverStatus.buildInfo); // start of build Info
+	sam_attach(ns,2261,UA_STRING, &sam.serverStatus.buildInfo.productName);
+	sam_attach(ns,2262,UA_STRING, &sam.serverStatus.buildInfo.productUri);
+	sam_attach(ns,2263,UA_STRING, &sam.serverStatus.buildInfo.manufacturerName);
+	sam_attach(ns,2264,UA_STRING, &sam.serverStatus.buildInfo.softwareVersion);
+	sam_attach(ns,2265,UA_STRING, &sam.serverStatus.buildInfo.buildNumber);
+	sam_attach(ns,2266,UA_DATETIME, &sam.serverStatus.buildInfo.buildDate);
+	sam_attach(ns,2992,UA_UINT32, &sam.serverStatus.secondsTillShutdown);
+	sam_attach(ns,2993,UA_STRING,&sam.serverStatus.shutdownReason);
+}
+
+UA_Int16 UA_NodeId_getNamespace(UA_NodeId const * id) {
 	return id->namespace;
 }
 // FIXME: to simple
-UA_Int16 UA_NodeId_getIdentifier(UA_NodeId* id) {
+UA_Int16 UA_NodeId_getIdentifier(UA_NodeId const * id) {
 	return id->identifier.numeric;
 }
-_Bool UA_NodeId_isBasicType(UA_NodeId* id) {
+
+_Bool UA_NodeId_isBasicType(UA_NodeId const * id) {
 	return (UA_NodeId_getNamespace(id) == 0) && (UA_NodeId_getIdentifier(id) <= UA_DIAGNOSTICINFO_NS0);
 }
 
@@ -119,13 +123,14 @@ UA_Int32 Namespace_getComponent(Namespace const * ns, UA_NodeId const * id, UA_I
 }
 
 
-UA_Int32 UAX_NodeId_encodeBinary(Namespace const * ns, UA_NodeId* id, UA_Int32* pos, UA_ByteString *dst) {
+UA_Int32 UAX_NodeId_encodeBinary(Namespace const * ns, UA_NodeId const * id, UA_Int32* pos, UA_ByteString *dst) {
 	UA_Int32 i, retval = UA_SUCCESS;
 	if (UA_NodeId_isBasicType(id)) {
-		void* data;
-		if ((retval = Namespace_getData(ns,id,&data,UA_NULL)) != UA_SUCCESS)
+		UA_Node const * result;
+		Namespace_Lock* lock;
+		if ((retval = Namespace_get(ns,id,&result,&lock)) != UA_SUCCESS)
 				return retval;
-		UA_[UA_ns0ToVTableIndex(UA_NodeId_getIdentifier(id))].encodeBinary(data,pos,dst);
+		UA_Variant_encodeBinary(&((UA_VariableNode *) result)->value,pos,dst);
 	} else {
 		UA_Int32 nComp = 0;
 		if ((retval = Namespace_getNumberOfComponents(ns,id,&nComp)) != UA_SUCCESS)
@@ -162,8 +167,9 @@ int main() {
 	DBG_VERBOSE(printf("aliases addr=%p, size=%d\n", (void*) &(n.aliases), n.aliases.size));
 	DBG_VERBOSE(UA_NodeSetAliases_println("aliases in nodeset: ", &n.aliases));
 
+	// Namespace_iterate(n.ns, print_node);
+	sam_init(n.ns);
 	Namespace_iterate(n.ns, print_node);
-	server_application_memory_init(n.ns);
 
 	// Direct encoding
 	UA_ByteString buffer = { 1024, (UA_Byte*) buf };
@@ -173,19 +179,26 @@ int main() {
 	nodeid.namespace = 0;
 	nodeid.identifier.numeric = 2256; // ServerStatus
 
-	// Design alternative one : use generated data types
-	void* data;
+	// Use generated data types if you are sure that the memory layout fits
+	UA_Node const * node;
+	Namespace_Lock* lock;
 	UA_Int32 pos = 0;
-	if (Namespace_getData(n.ns,&nodeid,&data,UA_NULL) != UA_SUCCESS)
+	if (Namespace_get(n.ns,&nodeid,&node,&lock) != UA_SUCCESS) {
 		perror("no data");
-	UA_ServerStatusDataType_encodeBinary(data,&pos,&buffer);
-	buffer.length = pos;
-	UA_ByteString_printx("generated encoder result=", &buffer);
+	} else {
+		if (node->nodeClass == UA_NODECLASS_VARIABLE) {
+			// I forgot how cool this is - the Variant simply knows how to handle itself
+			UA_Variant_encodeBinary(&((UA_VariableNode*) node)->value,&pos,&buffer);
+			buffer.length = pos;
+			UA_ByteString_printx("generated encoder result=", &buffer);
+		}
+		Namespace_Lock_release(lock);
+	}
 
-	// Design alternative two : use generated data types
+	// Design alternative two : use meta data
 	pos = 0;
 	buffer.length = 1024;
-	UAX_NodeId_encodeBinary(n.ns,&nodeid,&pos,&buffer);
+	// UAX_NodeId_encodeBinary(n.ns,&nodeid,&pos,&buffer);
 	buffer.length = pos;
 	UA_ByteString_printx("namespace based encoder result=", &buffer);
 
